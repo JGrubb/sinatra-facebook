e06e039e1e6dda2daa3fc74c486de8bb0ed06fa7
John Grubb <therealjohnnygrubb@gmail.com>
2012-06-25 18:07:28 -0400

    init commit

---
+++ facebook.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'net/http'
+require 'json'
+require 'sinatra'
+#require 'twitter'
+require 'haml'
+require 'redcarpet'
+
+def dat_render(text)
+  options = {
+    :autolink => true,
+    :space_after_headers => true,
+    :no_intra_emphasis => true,
+    :fenced_code_blocks => true
+  }
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+  markdown.render(text)
+end
+
+get '/' do
+  erb :index
+end
+
+get '/:band' do
+  fb = Net::HTTP.get_response("graph.facebook.com", "/#{params[:band]}")
+  @parsed = JSON.parse(fb.body)
+  erb :band
+end
