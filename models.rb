e87ada703a984de90d9001ac547c001ec1044879
John Grubb <therealjohnnygrubb@gmail.com>
2012-06-26 12:11:16 -0400

    Adding show model

--- models.rb
+++ models.rb
@@ -1,4 +1,9 @@
 class Band < ActiveRecord::Base
   validates :name, :twitter_user, :fb_user, :presence => true
   validates :name, :twitter_user, :fb_user, :uniqueness => true
+  has_many :shows
+end
+
+class Show < ActiveRecord::Base
+  belongs_to :band
 end
