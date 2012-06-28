bac234cc4c08f5db033556df6f0edf32e76eddec
John Grubb <therealjohnnygrubb@gmail.com>
2012-06-28 16:53:40 -0400

    commence Rakefile hacking, adding bones of Show and Tweets model

--- Rakefile
+++ Rakefile
@@ -1,2 +1,8 @@
 require 'sinatra/activerecord/rake'
 require './app'
+
+namespace :sobg do
+  desc "thrash out some rake tasks to do this background stuff"
+  task :pull_twitter do
+  end
+end
--- models.rb
+++ models.rb
@@ -2,8 +2,13 @@ class Band < ActiveRecord::Base
   validates :name, :twitter_user, :fb_user, :presence => true
   validates :name, :twitter_user, :fb_user, :uniqueness => true
   has_many :shows
+  has_many :tweets
 end
 
 class Show < ActiveRecord::Base
+  belongs_to :band
+end
+
+class Tweet < ActiveRecord::Base
   belongs_to :band
 end
